f3558f4c80e2d1cd3557e253bd475f8e651fb637
ouyangcm <mingorg@163.com>
2024-05-31 17:17:12 +0800

    新增模块

---
+++ app/src/main/java/com/org/oycm/pojo/User.java
@@ -0,0 +1,8 @@
+package com.org.oycm.pojo;
+
+/**
+ * @author ouyangcm
+ * create 2024/5/31 17:14
+ */
+public class User {
+}
--- src/main/java/com/org/oycm/Application.java
+++ src/main/java/com/org/oycm/Application.java
@@ -1,10 +1,16 @@
 package com.org.oycm;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author ouyangcm
  * create 2024/5/31 16:20
  */
 
-
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
