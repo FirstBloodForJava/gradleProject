e1e17a118a06ec2e3fab27a7cc7cf61b68023710
ouyangcm <mingorg@163.com>
2024-06-05 16:26:05 +0800

    Controller接口新增

--- src/main/java/com/org/oycm/controller/MyController.java
+++ src/main/java/com/org/oycm/controller/MyController.java
@@ -1,8 +1,37 @@
 package com.org.oycm.controller;
 
+import com.org.oycm.service.MyPrototypeService;
+import com.org.oycm.service.MySingletonService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author ouyangcm
  * create 2024/6/5 16:11
  */
+@RestController
 public class MyController {
+
+    private static final Log log = LogFactory.getLog(MyController.class);
+    @Autowired
+    MyPrototypeService myPrototypeService;
+
+    @Autowired
+    MySingletonService mySingletonService;
+
+    @GetMapping("getPrototype")
+    public String getPrototypeInfo(){
+        log.info("prototype: " + myPrototypeService);
+        return myPrototypeService.string();
+    }
+
+    @GetMapping("getSingleton")
+    public String getSingletonInfo(){
+        log.info("singleton: " + mySingletonService);
+        return mySingletonService.string();
+    }
+
 }
